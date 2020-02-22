acbf729dcbb1fd105d9535ab03f72e60d2a9c03b
Mathias Chicha <29279586+Mathias-C@users.noreply.github.com>
2020-02-22 20:17:25 +0100

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -7,9 +7,9 @@ echo "Installing core packages finished, downloading and installing NeoVim." && 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim ;\
 echo "Installing NeoVIM core packages finished, downloading and installing Anaconda 3." && \
-wget https://repo.anaconda.com/miniconda/Anaconda3-latest-Linux-x86_64.sh -O ~/Anaconda3.sh && \
-bash ~/Anaconda3.sh -b -p $HOME/Anaconda3 && \
-pip install neovim jedi flake8 autopep8 thefuck && \
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/Miniconda3.sh && \
+bash ~/Miniconda3.sh -b -p $HOME/Miniconda3 && \
+pip install pandas neovim jedi flake8 autopep8 thefuck awscli && \
 mkdir ~/.config/nvim/ && mv ./init.vim ~/.config/nvim/init.vim && \
 nvim +silent +VimEnter +PlugInstall +qall && \
 echo "Success installing all core packages"
